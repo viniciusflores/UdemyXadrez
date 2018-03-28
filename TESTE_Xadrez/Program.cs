6d16e2d37762b3dbcd0c623e1b132c27cb6b9981
Vinicius Flores <viniciusflores379@gmail.com>
2018-03-27 21:53:40 -0300

    começando a colocar peças no tabuleiro

--- TESTE_Xadrez/Program.cs
+++ TESTE_Xadrez/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
 namespace TESTE_Xadrez
 {
@@ -11,6 +12,12 @@ namespace TESTE_Xadrez
             //Console.WriteLine("posição: " + P);
 
             Tabuleiro tab = new Tabuleiro(8,8);
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+
+
+
             Tela.imprimirTabuleiro(tab);
 
             Console.ReadLine();
