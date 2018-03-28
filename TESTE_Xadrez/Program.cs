b4a664af787f2cdcf5b43a32a2db2d859ae4c4f6
Vinicius Flores <viniciusflores379@gmail.com>
2018-03-27 22:21:48 -0300

    implementado tratamento de erro posição inexistente e inválida

--- TESTE_Xadrez/Program.cs
+++ TESTE_Xadrez/Program.cs
@@ -8,18 +8,25 @@ namespace TESTE_Xadrez
     {
         static void Main(string[] args)
         {
-            //Posicao P = new Posicao(3, 4);
-            //Console.WriteLine("posição: " + P);
+            try
+            {
+                //Posicao P = new Posicao(3, 4);
+                //Console.WriteLine("posição: " + P);
 
-            Tabuleiro tab = new Tabuleiro(8,8);
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                Tabuleiro tab = new Tabuleiro(8, 8);
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                //tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(18, 9));
 
 
 
-            Tela.imprimirTabuleiro(tab);
-
+                Tela.imprimirTabuleiro(tab);
+            }
+            catch (TabuleiroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
             Console.ReadLine();
         }
     }
