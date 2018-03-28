0c74bbc4e73b4f10fdb82aae7901a14d4af83cf1
Vinicius Flores <viniciusflores379@gmail.com>
2018-03-27 23:33:15 -0300

    fazendo os primeiros movimentos

--- TESTE_Xadrez/Program.cs
+++ TESTE_Xadrez/Program.cs
@@ -10,26 +10,29 @@ namespace TESTE_Xadrez
         {
             try
             {
-                //Posicao P = new Posicao(3, 4);
-                //Console.WriteLine("posição: " + P);
-                Tabuleiro tab = new Tabuleiro(8, 8);
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-                //tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(18, 9));
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(2, 7));
-                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(5, 1));
+                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                Tela.imprimirTabuleiro(tab);
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+
+                    partida.executaMovimento(origem, destino);
+
+                }
+
             }
             catch (TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
             }
-            /*PosicaoXadrez pos = new PosicaoXadrez('a',1);
-            Console.WriteLine(pos);
-            Console.WriteLine(pos.toPosicao());*/
-                       
+            
 
 
             Console.ReadLine();
--- TESTE_Xadrez/tabuleiro/Peca.cs
+++ TESTE_Xadrez/tabuleiro/Peca.cs
@@ -15,6 +15,10 @@
             this.qteMovimentos = 0;
         }
         
+        public void incrementarQteMovimentos()
+        {
+            qteMovimentos++;
+        }
 
     }
 }
