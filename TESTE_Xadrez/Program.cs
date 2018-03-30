0d3ba0ab138fbe1bd0e7556ed6e777631610e03c
Vinicius Flores <viniciusflores379@gmail.com>
2018-03-29 22:42:49 -0300

    implementado a movimentação das pecas, apresenta mov possiveis após selecionar a peca

--- TESTE_Xadrez/Program.cs
+++ TESTE_Xadrez/Program.cs
@@ -20,6 +20,12 @@ namespace TESTE_Xadrez
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+
+                    bool[,] posicosPossiveis = partida.tab.peca(origem).movimentosPossiveis();
+
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab, posicosPossiveis);
+                    Console.WriteLine();
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
